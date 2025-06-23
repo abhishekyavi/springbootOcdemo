f8d1f9993ec7c933b5738cb6b2ad4aff06730bc1
Kumar <abhishek.aa.kumar@capgemini.com>
2025-06-23 19:48:40 +0530

    adding docker file

---
+++ src/main/java/com/java/oc/springbootOc/Controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.java.oc.springbootOc.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+    @PostMapping("/echo")
+    public String echoMessage(@RequestBody String message) {
+        return "Received: " + message;
+    }
+}
