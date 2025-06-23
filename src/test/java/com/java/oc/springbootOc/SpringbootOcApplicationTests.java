80cf0a83c38a1cdf4461f7cc53e24d5dab3feb9a
Kumar <abhishek.aa.kumar@capgemini.com>
2025-06-23 19:31:53 +0530

    init comit

---
+++ src/test/java/com/java/oc/springbootOc/SpringbootOcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.oc.springbootOc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
